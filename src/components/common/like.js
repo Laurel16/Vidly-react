525e08b014c95753805685550ad778e3e240391c
Laure Leter <laure.leter@gmail.com>
2020-11-24 18:44:29 +0100

    app

---
+++ src/components/common/like.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+
+
+export default class Like extends Component {
+  render() {
+    let classes = "fa fa-heart";
+    if (!this.props.liked) classes += "-o";
+
+    return (
+      <div>
+        <i onClick={this.props.onClick}
+        style={{cursor:"pointer"}}
+        className={classes}
+        aria-hidden="true"></i>
+      </div>
+    );
+  }
+}
---
+++ src/movies.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Movies extends Component {
+  state = { };
+
+  render() {
+    return
+    (<main className = "container">
+    <h1>Hello World</h1>
+    </main>);
+  }
+}
+
+export default Movies;
