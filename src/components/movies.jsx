1e1657a10a10330090dea3b5194296afeb7b3d66
Laure Leter <laure.leter@gmail.com>
2020-11-26 13:01:47 +0100

    auth && protectedRoutes

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -111,6 +111,7 @@ constructor()
       currentPage,
       sortColumn
     } = this.state;
+    const { user} = this.props;
 
     if (count === 0) return <p>There are no movies in the database. </p>;
 
@@ -125,13 +126,14 @@ constructor()
       onItemSelect={this.handleGenreSelect} />
       </div>
       <div className="col">
-      <Link 
+      { user && (<Link 
       to="/movies/new"
       className='btn btn-primary'
       style={{marginBottom:20}}
       >
         New movie
       </Link>
+      )}
       <p> Showing {totalCount} movies in the database. </p>
       <SearchBox value={searchQuery} onChange={this.handleSearch}/>
       <MoviesTable
